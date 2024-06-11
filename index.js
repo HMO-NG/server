451bddb097f1fc81e03815dedfaf9acf3197b347
wami ikechukwu <wamiikechukwu@gmail.com>
2024-06-11 14:23:40 +0100

    feat: added logger

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import router from './route.js'
 import KnexSessionStore from 'connect-session-knex'
-
+import morgan from 'morgan'
 import knex from "knex";
 import config from "./src/knexfile.js";
 
@@ -17,11 +17,11 @@ let db = knex(config[process.env.NODE_ENV || 'development']);
 
 const knexSession = KnexSessionStore(session);
 const store = new knexSession({
-    tablename:'sessions',
-    sidfieldname:'session_id',
-    knex:db,
-    createtable:true,
-    clearInterval:30000,
+    tablename: 'sessions',
+    sidfieldname: 'session_id',
+    knex: db,
+    createtable: true,
+    clearInterval: 30000,
 
 })
 
@@ -37,14 +37,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
     session({
         secret: process.env.SESSION_KEY,
-        resave:false,
-        saveUninitialized:false,
-        cookie:{
-            maxAge:900000
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 900000
         },
         store
     })
 )
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens['response-time'](req, res), 'ms'
+    ].join(' ')
+}))
 
 app.use('/api/v1', router)
 
