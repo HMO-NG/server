7453f820428ec7e2b01a0c9c0ca4d7090e706f25
wami ikechukwu <wamiikechukwu@gmail.com>
2024-06-23 12:10:58 +0100

    refactor: db schema

--- src/migrations/20240608114701_provider_table.cjs
+++ src/migrations/20240608114701_provider_table.cjs
@@ -5,13 +5,13 @@
 exports.up = function (knex) {
     return knex.schema.createTable('provider', table => {
         table.specificType('id', 'char(36) primary key');
-        table.string('name').notNullable();
+        table.string('name', 100).notNullable();
         table.string('email', 64).notNullable();
-        table.string('address', 64);
+        table.string('address');
         table.string('phone_number', 16);
         table.string('state', 16);
-        table.string('LGA', 16);
-        table.string('code', 16); //LAG/P/00001
+        table.boolean('status').defaultTo(1);
+        table.string('code', 16); //LAG/12345
         table.string('medical_director_name', 64);
         table.string('medical_director_phone_no', 16);
         table.string('created_by',36).notNullable().references('id').inTable('user').onDelete('cascade');
