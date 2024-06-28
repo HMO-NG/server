a6a549adeb67a73f62f41863749f98d2d3ecbb04
wami ikechukwu <wamiikechukwu@gmail.com>
2024-06-28 07:39:20 +0100

    wip: bulk upload

--- src/controller/nhis-service-controller.js
+++ src/controller/nhis-service-controller.js
@@ -7,39 +7,26 @@ const router = express.Router()
 // create health plan
 router.post('/nhis/services/create', auth, async (req, res, next) => {
 
-    try {
-        const data = req.body
+  try {
 
-        if (Array.isArray(data)) {
+    const data = req.body
 
-            for (let i = 0; i < data.length; i++) {
-            //   const batch = data.slice(1);
-              try {
-                // await Promise.all(batch.map(item => createNhisServiceModel(item)));
+    let result = await createNhisService(data)
 
-                console.log(`Processed batch ${i}`);
-                await createNhisService(data[i])
-              } catch (error) {
-                console.error(`Error processing batch ${i}`, error);
-              }
-            }
-          }
-
-        // let result = await createNhisService(data)
-
-        if (!result) {
-            throw new Exception("encountered an issue while creating nhis service", 401)
-        }
-
-        res.status(200).json({
-            message: `${data.name} created successfully`,
-            code: data.code
-        })
-    } catch (error) {
-        console.log(error.status)
-        next(error)
-
+    if (!result) {
+      throw new Exception("encountered an issue while creating nhis service", 401)
     }
+
+    res.status(200).json({
+      message: `${data.name} created successfully`,
+      code: data.code
+    })
+  } catch (error) {
+    console.log(error)
+    next(error)
+
+  }
 });
+
 
 export default router;
