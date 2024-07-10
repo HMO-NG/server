661f160f7317210ac00c20659dfbabc0c847f03c
wami ikechukwu <wamiikechukwu@gmail.com>
2024-07-10 16:43:05 +0100

    feat: created attached_benefit table migration file.

---
+++ src/migrations/20240710153058_attached_benefit.cjs
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable('attached_benefit', table => {
+        table.specificType('id', 'char(36) primary key');
+        table.string('benefit_name').notNullable();
+        table.string('limit_type', 35).notNullable();
+        table.boolean('limit_value', 20).notNullable();
+        table.string('health_plan_name', 36).notNullable();
+        table.timestamp('created_at').notNullable().defaultTo(knex.fn.now())
+        table.string('benefit_item_id', 36).notNullable().references('id').inTable('benefit_list').onDelete('cascade');
+        table.string('created_by', 36).notNullable().references('id').inTable('user').onDelete('cascade');
+        table.string('health_plan_id', 36).notNullable().references('id').inTable('health_plan').onDelete('cascade');
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable('attached_benefit')
+
+};
