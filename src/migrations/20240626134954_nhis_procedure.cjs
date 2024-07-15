6742aab2e6ff6b81081746b5c2f35d4e920beaba
wami ikechukwu <wamiikechukwu@gmail.com>
2024-07-15 13:28:40 +0100

    feat: updated migration file for nhis procuredure.

--- src/migrations/20240626134954_nhis_procedure.cjs
+++ src/migrations/20240626134954_nhis_procedure.cjs
@@ -7,14 +7,15 @@ exports.up = function (knex) {
 
         table.specificType('id', 'char(36) primary key');
         table.string('name').notNullable();
-        table.string('type', 20);
-        table.string('code', 20);
+        table.string('tarrif_type', 36);
+        table.string('service_type', 36);
+        table.string('nhia_code', 30);
         table.string('category');
         table.string('sub_category');
         table.double('price', 10, 2);
-        table.string('plan_type', 8).defaultTo('nhis');
+        table.string('plan_type', 8).notNullable().references('id').inTable('health_plan').onDelete('cascade');
         table.string('created_by', 36).notNullable().references('id').inTable('user').onDelete('cascade');
-        table.timestamp('created_at').notNullable().defaultTo(knex.fn.now())
+        table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
     })
 };
 
