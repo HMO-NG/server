36b8a87a7d71e58daea01e4b5888c606d65e29b8
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-02-02 19:53:49 +0100

    added company_id foreign key for private enrolle table

---
+++ src/migrations/20250202171440_private_enrollee_add_company_id_foreign_key.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.table('private_employees', table => {
+    table.foreign('company_id').references('user.id').onDelete('CASCADE');
+
+  });
+}
+
+/**
+* @param { import("knex").Knex } knex
+* @returns { Promise<void> }
+*/
+export async function down(knex) {
+  await knex.schema.table('private_employees',table =>{
+      table.dropForeign('company_id')
+  });
+}
