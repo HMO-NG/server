ddd117974331a40fb95925e031959d338f384696
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-02-02 19:48:20 +0100

    added foreign key for enrolled by field in company table

---
+++ src/migrations/20250202170600_private_enrollee_company_add_enrolled_by_foreign_key.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.table('private_company', table => {
+    table.foreign('enrolled_by').references('user.id').onDelete('CASCADE');
+
+  });
+}
+
+/**
+* @param { import("knex").Knex } knex
+* @returns { Promise<void> }
+*/
+export async function down(knex) {
+  await knex.schema.table('private_company',table =>{
+      table.dropForeign('enrolled_by')
+  });
+}
