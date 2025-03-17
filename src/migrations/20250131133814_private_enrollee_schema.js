c658660a02038290dbeb90098572aee466da688b
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-03-17 13:24:14 +0100

    added is active field and added medical data to the same table

--- src/migrations/20250131133814_private_enrollee_schema.js
+++ src/migrations/20250131133814_private_enrollee_schema.js
@@ -3,7 +3,7 @@
  * @returns { Promise<void> }
  */
 export async function up(knex) {
-  return knex.schema.createTable('private_employees', table => {
+  return knex.schema.createTable('enrollee', table => {
     table.string('id').primary(); // Use UUID for primary key
     table.string('first_name').notNullable();
     table.string('last_name').notNullable();
@@ -20,9 +20,19 @@ export async function up(knex) {
     table.string('state');
     table.string('city');
     table.string('address');
+
+    table.string('blood_group').nullable()
+    table.string('genotype').nullable()
+    table.string('disabilities').nullable()
+    table.string('allergies').nullable()
+    table.string('pre_existing_conditions').nullable()
+    table.string('past_surgeries').nullable()
+    table.string('family_medical_history');
+
     table.string('provider_id').nullable();
-    table.string('company_id').notNullable();
+    table.string('company_id').nullable();
     table.string('linked_to_user').nullable();
+    table.boolean('is_active').defaultTo(0);
     table.string('enrolled_by').notNullable(); //created by
     table.timestamp('modified_at').notNullable().defaultTo(knex.fn.now());
     table.timestamp('created_at').notNullable().defaultTo(knex.fn.now())
@@ -35,5 +45,5 @@ export async function up(knex) {
  * @returns { Promise<void> }
  */
 export async function down(knex) {
-  await knex.schema.dropTable('private_employees');
+  await knex.schema.dropTable('enrollee');
 }
