e5a81222673691df4d9d2283e543f1b7be7e39e1
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-04-13 18:44:14 +0100

    added documents router

--- src/route.js
+++ src/route.js
@@ -7,8 +7,9 @@ import getHealthCheck  from './controller/health-check-controller.js';
 import forms from './controller/form-controller.js';
 import privateCompany from './controller/private-company-controller.js';
 import privateEnrollees from './controller/private-enrollees-controller.js';
+import docs from './controller/document-controller.js'
 
-const router = [provider, auth, healthPlan, nhisService, nhisEnrollee, getHealthCheck, forms,privateCompany,privateEnrollees];
+const router = [provider, auth, healthPlan, nhisService, nhisEnrollee, getHealthCheck, forms,privateCompany,privateEnrollees,docs];
 
 export default router;
 
