aa404dcb27a9d4b21f5325a916ad9f481714a055
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-04-14 16:57:10 +0100

    migration for userid foreign key

---
+++ src/migrations/20250406193625_documents_add_linked_to_user_foreignkey.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.table('documents', table => {
+    table.foreign('user_id').references('user.id').onDelete('CASCADE');
+
+  });
+}
+
+/**
+* @param { import("knex").Knex } knex
+* @returns { Promise<void> }
+*/
+export async function down(knex) {
+  await knex.schema.table('documents',table =>{
+      table.dropForeign('user_id')
+  });
+}
