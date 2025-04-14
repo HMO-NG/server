d2dd555216632bde871fa46333e34098279dba0a
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-04-14 16:58:06 +0100

    migration for providerid foreign key

---
+++ src/migrations/20250411104441_provider_tarriff_add_provider_id_foreign_key.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.table('provider_service_tariff', table => {
+    table.foreign('provider_id').references('provider.id').onDelete('CASCADE');
+
+  });
+}
+
+/**
+* @param { import("knex").Knex } knex
+* @returns { Promise<void> }
+*/
+export async function down(knex) {
+  await knex.schema.table('provider_service_tariff',table =>{
+      table.dropForeign('provider_id')
+  });
+}
