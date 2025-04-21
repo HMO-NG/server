66ea1e2ef6e7d3b13a8d3f1c698c50cf062bfbff
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-04-21 08:00:48 +0100

    enable client account to be created

--- src/controller/private-company-controller.js
+++ src/controller/private-company-controller.js
@@ -5,21 +5,48 @@ import {
   getAllPrivateCompanyService,changePrivateCompanyStatusService,updateClientService,
 } from '../service/private-company-service.js';
 import Exception from '../util/exception.js';
+import generatePassword from '../util/generate-enrollee-password.js';
+import {
+createPrivateAccount
+} from '../service/private-enrollees-service.js';
 const router = express.Router()
 router.post('/privates/company/create', async (req, res, next) => {
     try {
 
         const data = req.body
+        let fname=data.company_name;
+        let lname=data.company_name;
+        let generated_password= generatePassword(`${fname} ${lname}`)
+        const user_data={
+          email: data.primary_contact_email,
+          first_name:data.company_name,
+          last_name: data.company_name,
+          phone_number: data.primary_contact_phonenumber,
+          password:generated_password,
+           role:"client",
+           type:"private",
+
+        }
+        const create_acc =await createPrivateAccount(user_data,next)
 
         let result = await createPrivateCompanyService(data)
+        let mainBody=`welcome to hci healthcare ${data.company_name}, these are your login details email: ${data.email} password: ${generated_password}`;
+        let subject = `Hci login details`;
+
+
+        email(mainBody,data.email,subject)
+        console.log(mainBody,data.primary_contact_email,subject)
 
         if (!result) {
             throw new Exception("encountered an issue while creating company", 400)
         }
-
+        if(result && create_acc){
         res.status(200).json({
             message: `${data.company_name} created successfully`,
+            data:{...result[0],
+            profile_id:create_acc.id}
         })
+      }
     } catch (error) {
         console.log(error)
         next(error)
