f0d0b80469acc8dfddf25613c8922c3c6af07d54
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-05-22 23:39:43 +0100

    added number of enrollees and other payment related fields

--- src/migrations/20250131133746_private_enrollee_company_schema.js
+++ src/migrations/20250131133746_private_enrollee_company_schema.js
@@ -12,8 +12,14 @@ export async function up (knex) {
     table.string('primary_contact_email').notNullable();
     table.string('primary_contact_phonenumber').notNullable();
     table.string('enrolled_by').notNullable(); // created by
+
+    table.decimal('number_of_enrollees').notNullable();
+    table.date('payment_start_date').notNullable();
+    table.date('payment_end_date').notNullable();
+    table.enum('payment_type',['weekly','monthly','quaterly','yearly','bi-annually','capitation']).defaultTo('monthly');
+
     table.boolean('is_active').defaultTo(1);
-    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now())
+    table.timestamps(true, true); // Adds created_at and updated_at
 
   });
 
