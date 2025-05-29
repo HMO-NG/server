5febb8234c394fce74666d41484945d92d84a812
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-05-29 11:40:11 +0100

    changed family_size to integer

--- src/migrations/20250131133814_private_enrollee_schema.js
+++ src/migrations/20250131133814_private_enrollee_schema.js
@@ -16,22 +16,17 @@ export async function up(knex) {
     table.string('position');
     table.date('dob');
     table.string('beneficiary_type').nullable();
-    table.string('family_size').nullable();
+    table.enum('enrollee_type',['primary','beneficiary']).nullable();
+    table.integer('family_size').nullable();
     table.string('state');
     table.string('city');
     table.string('address');
 
-    table.string('blood_group').nullable()
-    table.string('genotype').nullable()
-    table.string('disabilities').nullable()
-    table.string('allergies').nullable()
-    table.string('pre_existing_conditions').nullable()
-    table.string('past_surgeries').nullable()
-    table.string('family_medical_history');
-
+    table.string('health_plan_id')
     table.string('provider_id').nullable();
     table.string('company_id').nullable();
     table.string('linked_to_user').nullable();
+    table.string('primary_enrollee_id').nullable(); // the enrollee that made them thier beneficiary
     table.boolean('is_active').defaultTo(0);
     table.string('enrolled_by').notNullable(); //created by
     table.timestamp('modified_at').notNullable().defaultTo(knex.fn.now());
@@ -45,5 +40,7 @@ export async function up(knex) {
  * @returns { Promise<void> }
  */
 export async function down(knex) {
+ //await knex.raw('ALTER TABLE beneficiary DROP CONSTRAINT beneficiary_beneficiary_of_foreign');
+// await knex.raw('ALTER TABLE pre_authorization DROP CONSTRAINT pre_authorization_enrollee_id_foreign');
   await knex.schema.dropTable('enrollee');
 }
