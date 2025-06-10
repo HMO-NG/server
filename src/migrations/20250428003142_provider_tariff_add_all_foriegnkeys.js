c2f4bf1388fe6f531460f041e43e803c127e05e0
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-06-10 09:41:47 +0100

    feat(tariff):removed 'insurance_plan_id' foreign key from provider_tariff table

--- src/migrations/20250428003142_provider_tariff_add_all_foriegnkeys.js
+++ src/migrations/20250428003142_provider_tariff_add_all_foriegnkeys.js
@@ -5,7 +5,6 @@
 export async function up(knex) {
   await knex.schema.table('provider_tariff', table => {
     table.foreign('provider_id').references('provider.id').onDelete('CASCADE');
-    table.foreign('insurance_plan_id').references('health_plan.id').onDelete('CASCADE');
     table.foreign('created_by').references('user.id').onDelete('CASCADE');
 
   });
@@ -18,7 +17,6 @@ export async function up(knex) {
 export async function down(knex) {
   await knex.schema.table('provider_tariff',table =>{
       table.dropForeign('provider_id');
-      table.dropForeign('insurance_plan_id');
       table.dropForeign('created_by');
   });
 }
