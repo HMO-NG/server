34ef84864cff9ee014cad4e96bb66cb0bde7e1a0
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-06-25 23:06:57 +0100

    feat:added exra fields in tariff and made models to update pa by pa_code

--- src/model/provider-model.js
+++ src/model/provider-model.js
@@ -176,6 +176,7 @@ export async function getAllNhisProviderModel(data) {
 }
 //Create PROVIDER TARIFF
 export async function CreateProviderTariffModel(data) {
+  try{
   const tariff={
     id:uuidv4(),
     item_name:data.item_name,
@@ -207,6 +208,9 @@ export async function CreateProviderTariffModel(data) {
     }
         return new_tariff
   }
+   }catch(error){
+    console.error("Error updating provider tariff:", error);
+  }
 
 
 }
@@ -226,6 +230,7 @@ export async function getProviderTariffByIdModel(id) {
     'provider_tariff.category',
     db.raw(`"provider"."name" as "provider_name"`),
     db.raw(`concat("user"."first_name", \' \', "user"."last_name") as "created_by"`),
+    'provider_tariff.service_type',
     db.raw(`COALESCE(json_agg(
       DISTINCT jsonb_build_object(
         'id', health_plan.id,
@@ -267,6 +272,7 @@ export async function getAllProviderTariffModel() {
       'provider_tariff.category',
       db.raw(`"provider"."name" as "provider_name"`),
       db.raw(`concat("user"."first_name", \' \', "user"."last_name") as "created_by"`),
+      'provider_tariff.service_type',
       db.raw(`COALESCE(json_agg(
       DISTINCT jsonb_build_object(
         'id', health_plan.id,
@@ -309,6 +315,7 @@ export async function getSingleProviderTariffByIdModel(id) {
     'provider_tariff.category',
     db.raw(`"provider"."name" as "provider_name"`),
     db.raw(`concat("user"."first_name", \' \', "user"."last_name") as "created_by"`),
+    'provider_tariff.service_type',
     db.raw(`COALESCE(json_agg(
       DISTINCT jsonb_build_object(
         'id', health_plan.id,
@@ -342,11 +349,13 @@ export async function UpdateProviderTariffModel(id,data) {
     item_name:data.item_name,
     item_price:data.item_price,
     provider_id:data.provider_id,
-    insurance_plan_id:data.insurance_plan,
+    // insurance_plan_id:data.insurance_plan,
     hcpcs_code:data.hcpcs_code,
     tariff_type:data.tariff_type,
     is_surgical:data.is_surgical,
     patient_type:data.patient_type,
+    available_to_all_plans:data.available_to_all_plans,
+    service_type:data.service_type,
     category:data.category,
   }
   return await db('provider_tariff').where('id', id).update(tariff)
@@ -390,6 +399,7 @@ export async function getAllPreAuthorizationModel() {
     db.raw(`"provider"."code" as "provider_code"`),
     'pre_authorization.provider_id',
     'pre_authorization.provider_comment',
+    'pre_authorization.is_claimed',
     db.raw(`concat("user"."first_name", \' \', "user"."last_name") as "created_by"`),
     'pre_authorization.created_at',
 
@@ -424,6 +434,7 @@ export async function getPreAuthorizationByProviderIdModel(id) {
     db.raw(`"provider"."name" as "provider_name"`),
     db.raw(`"provider"."code" as "provider_code"`),
     'pre_authorization.provider_comment',
+    'pre_authorization.is_claimed',
     db.raw(`concat("user"."first_name", \' \', "user"."last_name") as "created_by"`),
     'pre_authorization.created_at',
 
@@ -460,6 +471,7 @@ export async function getSinglePreAuthorizationByIdModel(id) {
     db.raw(`"provider"."code" as "provider_code"`),
     'pre_authorization.provider_id',
     'pre_authorization.provider_comment',
+    'pre_authorization.is_claimed',
     db.raw(`concat("user"."first_name", \' \', "user"."last_name") as "created_by"`),
     'pre_authorization.created_at',
 
@@ -534,4 +546,21 @@ export async function getPATariffAndDiagnosisByCodeModel(PA_code) {
     console.error("Error fetching pre_authorization", error);
     throw error;
   }
+}
+
+export async function updatePreAuthorizationByPACodeModel(PA_code,data) {
+  const updatedData = {
+    requested_total_price:data.requested_total_price,
+    approved_price:data.approved_price,
+    diagnosis:data.diagnosis,
+    pa_code:data.pa_code,
+    selected_tariffs:data.selected_tariffs,
+    status:data.status,
+    provider_comment:data.provider_comment,
+    approved_date:data.approved_date,
+    denial_reason:data.denial_reason,
+    is_claimed:data.is_claimed,
+  }
+
+  return await db('pre_authorization').where('pa_code', PA_code).update(updatedData)
 }
