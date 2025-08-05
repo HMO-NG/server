717889eccf36c5f1ec3c24920f3a5b706cb629d2
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-08-05 09:35:56 +0100

    feat(band):get linked health plans instead of categories

--- src/model/band-model.js
+++ src/model/band-model.js
@@ -44,10 +44,10 @@ export async function getBandByIdModel(id) {
         db.raw(`concat("user"."first_name", \' \', "user"."last_name") as "created_by"`),
         db.raw(`COALESCE(json_agg(
               DISTINCT jsonb_build_object(
-                'id',health_plan_category.id,
-                'name', health_plan_category.name
+                'id',health_plan.id,
+                'name', health_plan.plan_name
               )
-          ) FILTER (WHERE health_plan_category.id IS NOT NULL), '[]') as linked_categories`),
+          ) FILTER (WHERE health_plan.id IS NOT NULL), '[]') as linked_plans`),
         db.raw(`COALESCE(json_agg(
               DISTINCT jsonb_build_object(
                 'id',provider.id,
@@ -56,9 +56,10 @@ export async function getBandByIdModel(id) {
           ) FILTER (WHERE provider.id IS NOT NULL), '[]') as linked_providers`)
       ).where('bands.id', id)
        .innerJoin('user', 'user.id', '=', 'bands.created_by')
-       .leftJoin('health_plan_category', 'health_plan_category.band_id', '=', 'bands.id')
-       .leftJoin('provider', 'provider.band_id', '=', 'bands.id')
-        .groupBy('bands.id', 'user.first_name', 'user.last_name',)
+       .leftJoin('health_plan', 'health_plan.band_id', '=', 'bands.id')
+       .leftJoin('provider_linked_bands', 'provider_linked_bands.band_id', '=', "bands.id")
+       .leftJoin('provider', 'provider.id', '=', 'provider_linked_bands.provider_id')
+       .groupBy('bands.id', 'user.first_name', 'user.last_name',)
        .first()
   } catch (error) {
       console.error(error)
