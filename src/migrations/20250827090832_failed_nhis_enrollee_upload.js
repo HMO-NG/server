71fcc2343d43842511e2000052ef0d97e5b59333
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-08-29 08:47:19 +0100

    feat:created db table schema for failed nhis enrollee uploads

---
+++ src/migrations/20250827090832_failed_nhis_enrollee_upload.js
@@ -0,0 +1,30 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+    return knex.schema.createTable('failed_nhis_enrollee', table => {
+        table.string('id').primary();
+        table.string('reason_for_failure');
+        table.dateTime('uploaded_at').defaultTo(knex.fn.now());
+        table.string('policy_id')
+        table.string('relationship');
+        table.string('surname');
+        table.string('other_names');
+        table.string('dob');
+        table.string('sex');
+        table.string('company_id');
+        table.string('provider_id');
+        table.string('provider_name');
+        table.string('provider_Address');
+
+    })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+    await knex.schema.dropTable('failed_nhis_enrollee');
+}
